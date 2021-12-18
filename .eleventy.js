38988d275788b10f6a8004bb373fee711dbb051a
Mathieu Huot <mathieu.huot@gmail.com>
2021-12-17 22:59:13 -0500

    Revert

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const fetch = require("cross-fetch");
 
 
 module.exports = config => {
